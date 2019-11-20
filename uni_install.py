35c6b3fa44a358cd4fe118ad6342bd3316e8ea9c
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2019-11-20 21:07:15 +0700

    `gitBuild()` now actually works

--- uni_install.py
+++ uni_install.py
@@ -3,6 +3,7 @@ import os
 import requests
 
 envSet = False
+gitSet = False
 
 def netTest(addr):
     try:
@@ -86,13 +87,35 @@ def dnfUpdate():
     subprocess.run(['sudo', 'dnf', 'update', '-y']) #Updates DB
     pass
 
+def setupBuildEnvGit():
+    global gitSet
+    os = getOS()
+    list_pkg = ['git', 'make']
+    if os == 'debian' or os == 'ubuntu':
+        list_pkg.append('build-essential')
+    elif os == 'fedora':
+        list_pkg.append('@development-tools')
+    elif os == 'arch':
+        list_pkg.append('base-devel')
+    systemInstall(list_pkg) #Install git
+    if chkBuildEnvGit(): 
+        gitSet = True
+        return True #Presumably Git is installed
+    else:
+        print("Failed to setup Git for building")
+        exit(1) #Something exploded. Stop.
+    pass
+
 def setupBuildEnvArch():
     global envSet
     list_pkg = ['git', 'base-devel'] #Nedded for Arch makepkg
     pacmanInstall(list_pkg) #Make damn sure build env is ready
     if chkBuildEnvArch():
         envSet = True
-        return True
+        return True #Presumably Git is installed
+    else:
+        print("Failed to setup Git for building")
+        exit(1) #Something exploded. Stop.
     pass
 
 def chkBuildEnvArch():
@@ -101,13 +124,13 @@ def chkBuildEnvArch():
     try:
         proc1 = subprocess.Popen(['git', 'help'], stdout=subprocess.DEVNULL)
         proc1.communicate()
-        proc1ret = proc1.returncode
+        proc1ret = proc1.returncode #Try to run git help. Returns 0 if git is there
     except FileNotFoundError:
         envSet = False
     try:
         proc2 = subprocess.Popen(['makepkg', '-h'], stdout=subprocess.DEVNULL)
         proc2.communicate()
-        proc2ret = proc2.returncode
+        proc2ret = proc2.returncode #Try to run makepkg -h. Returns 0 if makepkg is there
     except FileNotFoundError:
         envSet = False
     if (proc1ret == 0 and proc2ret == 0) or \
@@ -118,6 +141,27 @@ def chkBuildEnvArch():
         return False 
     pass
 
+def chkBuildEnvGit():
+    global gitSet
+    proc1ret, proc2ret = 69420, 177013
+    try:
+        proc1 = subprocess.Popen(['git', 'help'], stdout=subprocess.DEVNULL)
+        proc1.communicate()
+        proc1ret = proc1.returncode #Try to run git help. Returns 0 if git is there
+    except FileNotFoundError:
+        gitSet = False
+    try:
+        proc2 = subprocess.Popen(['make', '-h'], stdout=subprocess.DEVNULL)
+        proc2.communicate()
+        proc2ret = proc2.returncode #Try to run make -h. Returns 0 if makepkg is there
+    except FileNotFoundError:
+        gitSet = False
+    if (proc1ret == 0 and proc2ret == 0) or gitSet == True:
+        return True
+    else:
+        return False
+    pass
+
 def AURBuild(git_link: str):
     global envSet
     chkBuildEnvArch()
@@ -125,17 +169,23 @@ def AURBuild(git_link: str):
         setupBuildEnvArch()
         AURBuild(git_link)
     else:   
+        os.chdir("/tmp") #Go to tmp
+        subprocess.run(['git', 'clone', git_link]) #Clone the repo
+        tmpSpl = git_link.split("/")
+        os.chdir(tmpSpl[len(tmpSpl)-1].split(".")[0]) #Jump into the folder
+        subprocess.run(['makepkg', '-sci', '--noconfirm']) #make it
+    pass
+
+def gitBuild(git_link: str):
+    global gitSet
+    chkBuildEnvGit()
+    if gitSet == False:
+        setupBuildEnvGit()
+        gitBuild(git_link)
+    else:
         os.chdir("/tmp")
         subprocess.run(['git', 'clone', git_link])
         tmpSpl = git_link.split("/")
         os.chdir(tmpSpl[len(tmpSpl)-1].split(".")[0])
-        subprocess.run(['makepkg', '-sci', '--noconfirm'])
-    pass
-
-def gitBuild(git_link: str):
-    os.chdir("/tmp")
-    subprocess.run(['git', 'clone', git_link])
-    tmpSpl = git_link.split("/")
-    os.chdir(tmpSpl[len(tmpSpl)-1].split(".")[0])
-    subprocess.run(['make'])
-    subprocess.run(['sudo', 'make', 'install'])
+        subprocess.run(['make'])
+        subprocess.run(['sudo', 'make', 'install'])
